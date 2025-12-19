9f0928ea671c6a4ccd0a0842fea0ee3e173c3345
Thanury Tanisue <thanurytanisue@gmail.com>
2025-12-19 11:35:04 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,16 @@
+# idade = 15
+# print (idade > 18)
+
+# Operadores Significado
+# () Parenteses
+# ** Expoente
+# * Multiplicação
+# / Divisão
+# // Divisão por inteiros
+# % Modulus
+# +, - Adição, subtração
+# ==, !=, >, <, >=, <=, is, is not, in, not in Compadores lógicos, identidade,
+# pertencimento em conjuntos
+# not NOT booleano
+# and AND booleano
+# or OR booleano
