23956a75a585d3043cce9f5b451fa6c2c311bda4
Thanury Tanisue <thanurytanisue@gmail.com>
2025-12-19 12:21:58 -0300

    update

--- desenho_turtle.py
+++ desenho_turtle.py
@@ -1,16 +1,38 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
+# Cria a tela
+tela = Screen()
+tela.title("Mini Game Turtle")
+
+# Cria a turtle
 t = Turtle()
+t.speed(3)
 
-t.speed(1)
+# Loop principal
+continuar = 's'
 
-t.forward(100)
+while continuar == 's':
 
-t.right(90)
-t.forward(100)
+    # Movimento
+    direcao = input('Mover para frente ou para trás? (f/t): ').lower()
+    distancia = int(input('Quantos pixels deseja mover? '))
 
-t.left(90)
-t.forward(100)
+    if direcao == 'f':
+        t.forward(distancia)
+    elif direcao == 't':
+        t.backward(distancia)
 
-t.backward(200)
-input()
+    # Rotação
+    rotacao = input('Rotacionar para esquerda ou direita? (e/d): ').lower()
+    angulo = int(input('Quantos graus deseja rotacionar? '))
+
+    if rotacao == 'e':
+        t.left(angulo)
+    elif rotacao == 'd':
+        t.right(angulo)
+
+    # Continuar?
+    continuar = input('Continuar andando? (s/n): ').lower()
+
+# Mantém a tela aberta
+tela.exitonclick()
