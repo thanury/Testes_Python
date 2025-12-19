36cf5808f677dbd1e9227e636029593377089fb4
Thanury Tanisue <thanurytanisue@gmail.com>
2025-12-19 11:41:22 -0300

    loop

--- preco_corte_cabelo.py
+++ preco_corte_cabelo.py
@@ -4,14 +4,17 @@
 tamanho_cabelo_cm = 45
 
 # Condicional para definir o preço do corte
-if tamanho_cabelo_cm <= 20:
-    print('O valor do corte é R$50,00')
+# if tamanho_cabelo_cm <= 20:
+#     print('O valor do corte é R$50,00')
 
-elif tamanho_cabelo_cm <= 30:
-    print('O valor do corte é R$70,00')
+# elif tamanho_cabelo_cm <= 30:
+#     print('O valor do corte é R$70,00')
 
-elif tamanho_cabelo_cm <= 50:
-    print('O valor do corte é R$100,00')
+# elif tamanho_cabelo_cm <= 50:
+#     print('O valor do corte é R$100,00')
 
-else:
-    print('Acima de 50cm, favor consultar na recepção')
+# else:
+#     print('Acima de 50cm, favor consultar na recepção')
+
+idade = 25
+print('Maior de idade') if idade >=18 else print('Menor de idade')
