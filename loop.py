31b1372ccc475fcb3f550352a10a586c18ffb104
Thanury Tanisue <thanurytanisue@gmail.com>
2025-12-19 12:03:31 -0300

    while

--- loop.py
+++ loop.py
@@ -4,9 +4,28 @@
 # Loop Aninhados
 # País + Estação
 
-paises = ['brasil', 'india', 'estados unidos']
-estacoes_do_ano = ['primavera', 'verão', 'outono', 'inverno']
+# paises = ['brasil', 'india', 'estados unidos']
+# estacoes_do_ano = ['primavera', 'verão', 'outono', 'inverno']
 
-for pais in paises:
-    for estacao in estacoes_do_ano:
-        print(f'{pais} {estacao}')
+# for pais in paises:
+#     for estacao in estacoes_do_ano:
+#         print(f'{pais} {estacao}')
+
+# contador = 1
+
+# while contador <= 120:
+#     print(contador)
+#     contador += 1
+
+# senha = ''
+
+# while senha != 'secreto':
+#     senha = input('Digite a senha: ')
+
+# print('Acesso liberado')
+
+contador = 100
+
+while contador >= 1:
+    print(contador)
+    contador -= 1
